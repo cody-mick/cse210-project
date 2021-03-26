fa73020fff0e8e6ea2c571deb678b7dcf3f49495
Samuel Cummings <cummings.samuel007@gmail.com>
2021-03-25 23:36:13 -0400

    random updates

--- project_template/game/__main__.py
+++ project_template/game/__main__.py
@@ -8,6 +8,7 @@ from virus_cells import Virus_cells
 from solid_blocks import Solid_blocks
 import math
 import random
+from menu import Menu
 
 class MyGame(arcade.View):
     """ Main application class. """
@@ -210,25 +211,6 @@ class MyGame(arcade.View):
         self.bullet_list.update()
 
                 
-class Menu(arcade.View):
-     """ Class that manages the 'menu' view. """
-
-     def on_show(self):
-        """ Called when switching to this view"""
-        # arcade.load_texture(":resources:images/backgrounds/abstract_1.jpg")
-        arcade.set_background_color(arcade.color.WHITE)
-
-     def on_draw(self):
-        """ Draw the menu """
-        arcade.start_render()
-        arcade.draw_text("Menu Screen - click to advance", constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2,
-                         arcade.color.BLUE, font_size=30, anchor_x="center")
-
-     def on_mouse_press(self, _x, _y, _button, _modifiers):
-        """ Use a mouse press to advance to the 'game' view. """
-        game_view = MyGame()
-        game_view.setup()
-        self.window.show_view(game_view)
 
 def main():
     """ Main method """
